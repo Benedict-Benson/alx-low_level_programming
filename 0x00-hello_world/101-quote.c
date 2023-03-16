83b54914b8a2026050179d611fd9d71c38d54c86
Benedict-Benson <Benedict-Benson>
2023-03-16 16:20:47 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistdio.h>
+
+/**
+ * main - program that prints exactly
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+fput(-and that piece of art is useful\" - Dora Korpar, 2015-10-19\n-,stdout);
+return (1)
+}
