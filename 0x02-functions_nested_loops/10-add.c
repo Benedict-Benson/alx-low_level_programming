1d259fed502536b5cf071fb591efabad46b90cd7
Benedict-Benson <Benedict-Benson>
2023-03-21 12:58:40 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - addition 
+ * @m: int to be added to n
+ * @n: int to be added to m
+ *
+ * Return: sum of two integers
+ */
+int add(int m, int n)
+{
+	int i;
+
+	i = m + n;
+
+	return (i);
+}
