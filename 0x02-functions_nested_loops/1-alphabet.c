f8c6ef7e71462544c7fb403f9db36f13ee0330a8
Benedict-Benson <Benedict-Benson>
2023-03-21 10:17:57 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints alphabets in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		_putchar(alp);
+
+	_putchar('\n');
+
+}
