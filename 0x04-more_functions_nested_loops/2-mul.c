639c726d81a30fd85d0d3f03c192849efd236556
Benedict-Benson <Benedict-Benson>
2023-03-23 13:33:00 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,17 @@
 include "main.h"
+
 /**
  * mul - multiply 2 int
  * @a: first int
  * @b: second int
  * Return: addition of the multiplicattion
  */
-
 int mul(int a, int b)
 {
-	return (a * b);
-}
 
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
