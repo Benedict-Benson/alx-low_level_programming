b9dd385c341850ea02ff0b7ea58d96b88fe944d9
Benedict-Benson <Benedict-Benson>
2023-03-23 13:17:04 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,34 +1,35 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - prints the numbers from 1 to 100
- * Return: Always 0 (Success)
+ * main -  checks for checks for a digit (0 through 9).
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i;
+	int x;
 
-	for (i = 1; i <= 100; i++)
+	for (x = 1; x <= 100; x++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if (x % 3 == 0 || x % 5 == 0)
 		{
-			printf(" Fizz");
-		} else if (i % 5 == 0 && i % 3 != 0)
+			if (x % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (x % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		}
+		else
 		{
-			printf(" Buzz");
-		} else if (i % 3 == 0 && i % 5 == 0)
+			printf("%d", x);
+		}
+		if (x != 100)
 		{
-			printf(" FizzBuzz");
-		} else if (i == 1)
-		{
-			printf("%d", i);
-		} else
-		{
-			printf(" %d", i);
+		putchar(' ');
 		}
 	}
-	printf("\n");
-
+	putchar('\n');
 	return (0);
-
+}
