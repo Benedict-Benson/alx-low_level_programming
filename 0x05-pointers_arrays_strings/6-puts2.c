52409bb581920b38ffe564b9ff49daf8355b0187
Benedict-Benson <Benedict-Benson>
2023-03-27 12:39:05 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,11 @@
 #include "main.h"
-
 /**
- * puts2 - function print only one character out of two
+ * puts2 - function should print only one character out of two
  * starting with the first one
  * @str: input
  * Return: print
  */
+void puts2(char *str)
 {
 	int longi = 0;
 	int t = 0;
@@ -27,3 +27,4 @@
 	}
 	_putchar('\n');
 }
+
