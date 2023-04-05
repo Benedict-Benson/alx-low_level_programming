e57cc993ef5d4b2b33f8f30e8015fd6b593a3737
Benedict-Benson <Benedict-Benson>
2023-04-05 12:50:31 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,24 +10,22 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (my_sqrt_recursion(n, 0));
+	return (sqrt_recursion(n, 0));
 }
 
 /**
- * my_sqrt_recursion - recurses to find the natural
+ * sqrt_recursion - recurses to find the natural
  * square root of a number
  * @n: number
  * @i: iterator
  *
- *
  * Return: the resulting square root
  */
-int my_sqrt_recursion(int n, int i)
+int sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (my_sqrt_recursion(n, i + 1));
+	return (sqrt_recursion(n, i + 1));
 }
-
