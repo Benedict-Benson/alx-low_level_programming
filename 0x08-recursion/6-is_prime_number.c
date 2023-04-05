c744627b1725ce707f009d89a3d441c1c703198a
Benedict-Benson <Benedict-Benson>
2023-04-05 13:16:55 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * is_prime_number - 1 if the input integer is
+ * a prime number, otherwise return 0
+ * @n: int
+ *
+ * Return: prime numbers
+ */
+int is_prime_number(int n)
+{
+	if (n <= 0)
+		return (0);
+	return (pnum(n, 2));
+}
+
+/**
+ * pnum - real prime number
+ * @n: int
+ * @m: iterator
+ *
+ * Return: prime number
+ */
+int pnum(int n, int m)
+{
+	if (n == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
+]
