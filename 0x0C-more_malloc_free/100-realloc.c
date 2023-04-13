fc3bf0d69d7a491fae0252ee54062018ead265f0
Benedict-Benson <Benedict-Benson>
2023-04-13 12:15:58 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,12 +11,14 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *ptr;
+
+	char *ptr1;
 	char *old_ptr;
 	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
+
 	if (new_size == 0 && ptr)
 	{
 		free(ptr);
@@ -25,11 +27,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (!ptr)
 		return (malloc(new_size));
+
 	ptr1 = malloc(new_size);
 	if (!ptr1)
 		return (NULL);
 
 	old_ptr = ptr;
+
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
