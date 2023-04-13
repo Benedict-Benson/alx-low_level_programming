bcaf230f81d661de277779f503a3078df1800998
Benedict-Benson <Benedict-Benson>
2023-04-13 11:25:37 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: unsigned int
+ * @size: unsigned int
+ *
+ * Return: a pointer to the allocated memory.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	a = malloc(nmemb * size);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		q[i] = 0;
+
+	return (q);
+}
