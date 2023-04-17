37203fa90977c5e8c99f39a56e0aaaa3f0196973
Benedict-Benson <Benedict-Benson>
2023-04-17 12:02:15 -0700

    function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _ABS_FILE_H_
-#define _ABS_FILE_H_
+#ifndef _ABS_H_
+#define _ABS_H_
 
-#define ABS(x) ((x > (0) ? (x) : (x)) * (-1))
+#define ABS(x) ((x) > (0) ? (x) : (x) * (-1))
 
 #endif
