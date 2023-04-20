f86604018cb1cbc7f29dd5fe919d6945e3e790fc
Benedict-Benson <Benedict-Benson>
2023-04-20 13:33:23 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		stn = va_arg(string, char *);
 
 		if (stn == NULL)
-			printf("(nill)");
+			printf("(nil)");
 
 		else
 			printf("%s", stn);
