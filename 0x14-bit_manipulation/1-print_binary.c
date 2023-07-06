a4109af3bf4bca69bb24fbd3025e710fbbfbf354
Benedict Benson <bndctbnson@gmail.com>
2023-07-06 07:34:43 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_binary - fn that prints binary No
+ * @n: integer
+ * Return: Binary No
+ */
+void print_binary(unsigned long int n)
+{
+	int No;
+	static int count;
+
+	if (n == 0 && count > 0)
+		return;
+	else if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	No = (n & 1);
+	count++;
+
+	print_binary(n >>= 1);
+	_putchar('0' + No);
+}
+
