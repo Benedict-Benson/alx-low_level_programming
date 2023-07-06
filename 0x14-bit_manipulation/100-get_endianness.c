9062a31e5c772c3c460e21a8a2f0708e7ea8ed23
Benedict Benson <bndctbnson@gmail.com>
2023-07-06 14:04:11 -0700

    bit manipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,15 +2,16 @@
 #include "main.h"
 
 /**
- * get_endianness - checks endianness
- * return: 0 if big endian, 1 if little endian
+ * get_endianness - checks the endiannes
+ *
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
 	unsigned int i = 1;
 	char *a;
 
-	a = (char *)&i;	
+	a = (char *)&i;
 
 	return ((int)*a);
 }
