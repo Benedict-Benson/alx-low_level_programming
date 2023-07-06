1d02bda496eb8162a26d787779c293eb3eb37cd5
Benedict Benson <bndctbnson@gmail.com>
2023-07-06 07:05:25 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - function conerts binary to number
+ * b: pointing to a string of 1 and 0
+ * Return: the conerted No
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b)
+	{
+		if (*b == '1')
+			decimal = (decimal << 1) | 1;
+		else if (*b == '0')
+		       decimal <<= 1;
+		else
+			return (0);
+		b++;
+	}
+
+	return (decimal);
+}
+
+
